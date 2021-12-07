426e3ddae068c1cd63409366bd2ec8de8e689ec5
vipin.kumar@mtn.com <vipin.kumar@mtn.com>
2021-12-07 22:12:31 +0530

    sorting added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,10 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +19,10 @@ import { BookListComponent } from './book-list/book-list.component';
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,24 +1,59 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort, Sort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 import { BookListService } from '../book-list.service';
+
+export interface BookList {
+  title: string;
+  description: string;
+  publishDate: string;
+
+}
 
 @Component({
   selector: 'app-book-list',
   templateUrl: './book-list.component.html',
   styleUrls: ['./book-list.component.css']
 })
+
 export class BookListComponent implements OnInit {
   books: any;
+  sortedData: any;
 
-  constructor(private bookListService: BookListService) { }
-
-  ngOnInit(): void {
+  constructor(private bookListService: BookListService) {
     this.getBooks();
   }
+
+  ngOnInit(): void { }
 
   getBooks(): void {
     this.books = this.bookListService.getBooks().subscribe((data: any) => {
       this.books = data;
+      this.sortedData = data;
     })
   }
 
+  sortData(sort: Sort) {
+    const data = this.books.slice();
+    if (!sort.active || sort.direction === '') {
+      this.sortedData = data;
+      return;
+    }
+
+    this.sortedData = data.sort((a: any, b: any) => {
+      const isAsc = sort.direction === 'asc';
+      switch (sort.active) {
+        case 'title':
+          return compare(a.title, b.title, isAsc);
+        case 'publishDate':
+          return compare(a.publishDate, b.publishDate, isAsc);
+        default:
+          return 0;
+      }
+    });
+  }
+}
+function compare(a: number | string, b: number | string, isAsc: boolean) {
+  return (a < b ? -1 : 1) * (isAsc ? 1 : -1);
 }
